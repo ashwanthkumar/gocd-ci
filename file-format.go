bc5153baa2438e539348ec08115afb014898f2d8
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-03-13 15:57:30 +0530

    Setting env to empty map instead of nil when it's not specified in the spec

--- file-format.go
+++ file-format.go
@@ -14,10 +14,17 @@ type CISpec struct {
 }
 
 func DecodeCISpec(contents string) (CISpec, error) {
+	return DecodeCISpecFromBytes([]byte(contents))
+}
+
+func DecodeCISpecFromBytes(data []byte) (CISpec, error) {
 	ciSpec := CISpec{}
-	err := yaml.Unmarshal([]byte(contents), &ciSpec)
+	err := yaml.Unmarshal(data, &ciSpec)
 	if ciSpec.Cmd == nil {
 		err = fmt.Errorf("Required field `cmd` is missing")
+	}
+	if ciSpec.Env == nil {
+		ciSpec.Env = make(map[string]string)
 	}
 	return ciSpec, err
 }
--- file-format_test.go
+++ file-format_test.go
@@ -29,6 +29,29 @@ artifacts: {}
 	assert.Equal(t, expectedSpec, spec)
 }
 
+func TestDecodeCISpecFromBytes(t *testing.T) {
+	var sampleSpec = `
+name: "GoCD-CI"
+env:
+  FOO: bar
+  BAR: baz
+  BAZ: day
+cmd:
+  - echo "Hello World"
+artifacts: {}
+`
+	spec, err := DecodeCISpecFromBytes([]byte(sampleSpec))
+	assert.Nil(t, err)
+	expectedSpec := CISpec{
+		Name:      "GoCD-CI",
+		Env:       map[string]string{"FOO": "bar", "BAR": "baz", "BAZ": "day"},
+		Cmd:       []string{"echo \"Hello World\""},
+		Artifacts: make(map[string]string),
+	}
+
+	assert.Equal(t, expectedSpec, spec)
+}
+
 func TestDecodeCISpecWithoutArtifacts(t *testing.T) {
 	var sampleSpec = `
 name: "GoCD-CI"
@@ -61,6 +84,7 @@ cmd:
 	expectedSpec := CISpec{
 		Name: "GoCD-CI",
 		Cmd:  []string{"echo \"Hello World\""},
+		Env:  make(map[string]string),
 	}
 
 	assert.Equal(t, expectedSpec, spec)
